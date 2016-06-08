a5f3a2cb65edd4defa548aca5f4a74280c6b8c11
Todor Mitev <siper92@gmail.com>
2016-06-08 21:23:05 +0300

    Added odd-even-chars for codding chalange

--- src/clojure_learning/odd_even_chars.clj
+++ src/clojure_learning/odd_even_chars.clj
@@ -1,1 +1,19 @@
 (ns clojure-learning.odd-even-chars)
+
+(defn separate-even-and-odd-chars
+  [word]
+  (loop [i 0 wordLenght (count word) parts {:odd "" :even ""}]
+    (let [type (if (not (odd? i))
+                 :odd
+                 :even)]
+      (if (> i (count word))
+        parts
+        (recur (+ i 1) wordLenght (assoc parts type (str (get parts type) (get word i))))))))
+
+(defn -main [& args]
+  ; (let lines (line-seq (java.io.BufferedReader. *in*)))
+  (let [N (let lines (line-seq (java.io.BufferedReader. *in*))) words (rest N)]
+    (doseq [word words]
+      (let [wordLenght (count word)]
+        (let [wordParts (separate-even-and-odd-chars word)]
+          (println (format "%s %s" (get wordParts :odd) (get wordParts :even))))))))
